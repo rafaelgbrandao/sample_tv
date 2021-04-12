8b18c6577d70391b7f08ae1ed271d2f0345dc008
rafael.godinho <rafagbr@hotmail.com>
2021-04-12 13:38:18 -0300

    Initial commit

---
+++ app/src/main/java/com/littlegod/sampletv/Sample.kt
@@ -0,0 +1,12 @@
+package com.littlegod.sampletv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Sample: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample)
+    }
+}
