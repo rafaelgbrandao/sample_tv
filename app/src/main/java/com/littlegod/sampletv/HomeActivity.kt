f66241db1103efb4c3731fb33b4e7b8dc0723ce5
rafael.godinho <rafagbr@hotmail.com>
2021-04-14 10:36:35 -0300

    Add Home activity

--- app/src/main/java/com/littlegod/sampletv/HomeActivity.kt
+++ app/src/main/java/com/littlegod/sampletv/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.littlegod.sampletv
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Sample: AppCompatActivity() {
+class HomeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sample)
+        setContentView(R.layout.activity_home)
     }
 }
